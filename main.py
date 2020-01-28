23a44a329433833505238c485a0e97ea560c4f5d
P2LOVE <40074412+P2LOVE@users.noreply.github.com>
2020-01-29 01:19:18 +0400

    fix notifications + comma separating

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ vkToken = ""
 # Сюда вставляешь свой токен (Я юзал от кейта. Получить можно тут https://vkhost.github.io )
 
 
-my_id = 578716413  # Здесь вводишь свой айди (прим. my_id = 1)
+my_id =   # Здесь вводишь свой айди (прим. my_id = 1)
 
 contest_trigger_list = ()  # Листик слов триггеров для уведомления о розыгрыше:
 # Прим. contest_trigger_list = ('розыгрыш', 'конкурс')
@@ -110,11 +110,11 @@ def contest_updater(cu_id):
                 peer_id=cu_id,
                 message_id=contest_msg_id.get(cu_id),
                 message="Ого! Запущено начало розыгрыша. \nДля принятия участия введите: "
-                + contest_instruction_row.get(cu_id)
-                + "\n\n До окончания розыгрыша: "
-                + str(setup_timer.get(cu_id))
-                + "мин.\n\nУчастники в розыгрыше: "
-                + ", ".join(contest_member(cu_id)),
+                        + contest_instruction_row.get(cu_id)
+                        + "\n\n До окончания розыгрыша: "
+                        + str(setup_timer.get(cu_id))
+                        + " мин.\n\nУчастники в розыгрыше: "
+                        + ", ".join(contest_member(cu_id)),
             )
         except vk_api.exceptions.ApiError:
             contest_cleaner(cu_id)
@@ -138,8 +138,8 @@ def contest_updater(cu_id):
                         peer_id=cu_id,
                         random_id=0,
                         message="В розыгрыше побеждает "
-                        + random.choice(contest_member(cu_id))
-                        + "\nПоздравляем!",
+                                + random.choice(contest_member(cu_id))
+                                + "\nПоздравляем!",
                         reply_to=contest_msg_id.get(cu_id),
                     )
                     contest_cleaner(cu_id)
@@ -150,71 +150,77 @@ def contest_updater(cu_id):
 
 for event in longpoll.listen():
     if (
-        event.type == VkEventType.MESSAGE_NEW
-        and event.text.lower().startswith(trigger_word)
-        and event.from_me
-        and len(event.text.split()) == 1
+            event.type == VkEventType.MESSAGE_NEW
+            and event.text.lower().startswith(trigger_word)
+            and event.from_me
+            and len(event.text.split()) == 1
     ):
         if len(event.text) > len(trigger_word):
-            if event.text[len(trigger_word) :] == "1":
+            if event.text[len(trigger_word):] == "1":
                 to_delete_count = 2
                 msg_delete()
             else:
-                if event.text[len(trigger_word) :].isdigit():
-                    to_delete_count = int(event.text[len(trigger_word) :]) + 1
+                if event.text[len(trigger_word):].isdigit():
+                    to_delete_count = int(event.text[len(trigger_word):]) + 1
                     msg_delete()
         else:
             to_delete_count = 2
             msg_delete()
     if (
-        event.type == VkEventType.MESSAGE_NEW
-        and event.text.lower().startswith(trigger_word + "-")
-        and event.from_me
-        and len(event.text.split()) is 1
+            event.type == VkEventType.MESSAGE_NEW
+            and event.text.lower().startswith(trigger_word + "-")
+            and event.from_me
+            and len(event.text.split()) is 1
     ):
         if len(event.text) > (len(trigger_word) + 1):
-            if event.text[(len(trigger_word) + 1) :] == "1":
+            if event.text[(len(trigger_word) + 1):] == "1":
                 to_delete_count = 2
                 msg_replace_delete()
             else:
-                if event.text[(len(trigger_word) + 1) :].isdigit():
-                    to_delete_count = int(event.text[(len(trigger_word) + 1) :]) + 1
+                if event.text[(len(trigger_word) + 1):].isdigit():
+                    to_delete_count = int(event.text[(len(trigger_word) + 1):]) + 1
                     msg_replace_delete()
         else:
             to_delete_count = 2
             msg_replace_delete()
     if (
-        event.type == VkEventType.MESSAGE_NEW
-        and event.from_chat
-        and any(
-            contest_trigger_word in event.text.lower()
-            for contest_trigger_word in contest_trigger_list
-        )
+            event.type == VkEventType.MESSAGE_NEW
+            and event.from_chat
+            and any(
+                contest_trigger_word in event.text.lower()
+                for contest_trigger_word in contest_trigger_list
+            )
     ):
-        if event.user_id in contest_white_list:
+        if (
+                type(contest_white_list) is tuple
+                and event.user_id in contest_white_list
+        ) or (
+                type(contest_white_list) is int
+                and event.user_id == contest_white_list
+        ):
             vk.messages.send(
                 peer_id=my_id,
                 random_id=0,
                 message=f"Потенциальное начало конкурса/розыгрыша в"
-                f" {vk.messages.getChatPreview(peer_id=event.peer_id).get('preview')['title']}"
-                f"\n\n{vk.users.get(user_ids=my_id)[0].get('first_name')}"
-                f" не пропусти его, котик ♥",
+                        f" {vk.messages.getChatPreview(peer_id=event.peer_id).get('preview')['title']}"
+                        f"\n\n{vk.users.get(user_ids=my_id)[0].get('first_name')}"
+                        f" не пропусти его, котик ♥",
                 forward_messages=event.message_id,
             )
     if (
-        event.type == VkEventType.MESSAGE_NEW
-        and event.text.lower().startswith(start_my_contest_trigger)
-        and event.from_me
-        and event.from_chat
-        and (
+            event.type == VkEventType.MESSAGE_NEW
+            and event.text.lower().startswith(start_my_contest_trigger)
+            and event.from_me
+            and event.from_chat
+            and (
             not started_contest.get(event.peer_id)
             or started_contest.get(event.peer_id) is None
-        )
+            )
     ):
         if (
-            len(event.text) > len(start_my_contest_trigger)
-            and event.text.split()[0].isdigit()
-            and event.text.split()[1] != "0"
+                len(event.text) > len(start_my_contest_trigger)
+                and event.text.split()[1].isdigit()
+                and event.text.split()[1] != "0"
         ):
             setup_timer.update({event.peer_id: int(event.text.split()[1])})
             vk.messages.delete(message_ids=event.message_id, delete_for_all=1)
@@ -222,14 +228,14 @@ for event in longpoll.listen():
                 contest_instruction.update(
                     {
                         event.peer_id: " ".join(
-                            event.text.split()[2 : len(event.text.split())]
+                            event.text.split()[2: len(event.text.split())]
                         ).lower()
                     }
                 )
                 contest_instruction_row.update(
                     {
                         event.peer_id: " ".join(
-                            event.text.split(" ")[2 : len(event.text.split())]
+                            event.text.split(" ")[2: len(event.text.split())]
                         )
                     }
                 )
@@ -237,19 +243,19 @@ for event in longpoll.listen():
                     peer_id=event.peer_id,
                     random_id=0,
                     message=f"Ого! Запущено начало розыгрыша."
-                    f"\nДля принятия участия введите:"
-                    f" {contest_instruction_row.get(event.peer_id)}"
-                    f" \n\n До окончания розыгрыша:"
-                    f" {setup_timer.get(event.peer_id)}"
-                    f" мин.\n\nУчастники в розыгрыше:",
+                            f"\nДля принятия участия введите:"
+                            f" {contest_instruction_row.get(event.peer_id)}"
+                            f" \n\n До окончания розыгрыша:"
+                            f" {setup_timer.get(event.peer_id)}"
+                            f" мин.\n\nУчастники в розыгрыше:",
                 )
             except IndexError:
                 contest_cleaner(event.peer_id)
     if (
-        event.type == VkEventType.MESSAGE_NEW
-        and event.from_me
-        and event.text.startswith("Ого!")
-        and started_contest.get(event.peer_id) is None
+            event.type == VkEventType.MESSAGE_NEW
+            and event.from_me
+            and event.text.startswith("Ого!")
+            and started_contest.get(event.peer_id) is None
     ):
         contest_peer_id.update({contest_instruction.get(event.peer_id): event.peer_id})
         contest_msg_id.update({event.peer_id: event.message_id})
@@ -258,9 +264,9 @@ for event in longpoll.listen():
         started_contest.update({event.peer_id: True})
         threading.Thread(target=contest_updater, args=(event.peer_id,)).start()
     if (
-        event.type == VkEventType.MESSAGE_NEW
-        and event.from_chat
-        and contest_validator()
+            event.type == VkEventType.MESSAGE_NEW
+            and event.from_chat
+            and contest_validator()
     ):
         if event.user_id not in contest_member_list.get(event.peer_id):
             contest_member_list.get(event.peer_id).append(event.user_id)
@@ -269,12 +275,12 @@ for event in longpoll.listen():
                     peer_id=event.peer_id,
                     message_id=contest_msg_id.get(event.peer_id),
                     message=f"Ого! Запущено начало розыгрыша."
-                    f" \nДля принятия участия введите:"
-                    f" {contest_instruction_row.get(event.peer_id)}"
-                    f" \n\n До окончания розыгрыша:"
-                    f" {setup_timer.get(event.peer_id)}"
-                    f" мин.\n\nУчастники в розыгрыше:"
-                    f" {''.join(contest_member(event.peer_id))}",
+                            f" \nДля принятия участия введите:"
+                            f" {contest_instruction_row.get(event.peer_id)}"
+                            f" \n\n До окончания розыгрыша:"
+                            f" {setup_timer.get(event.peer_id)}"
+                            f" мин.\n\nУчастники в розыгрыше:"
+                            f" {', '.join(contest_member(event.peer_id))}",
                 )
             except vk_api.exceptions.ApiError:
                 contest_cleaner(event.peer_id)
